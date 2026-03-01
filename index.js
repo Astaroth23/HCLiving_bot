50dc5e736d48dfc4373b60e0fe1831dac20febef
Astaroth23 <lucio.baglio@gmail.com>
2026-03-01 04:07:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -446,12 +446,3 @@ try {
   console.log("Another instance detected, exiting.");
   process.exit(0);
 }
-
-const PORT = process.env.PORT || 10000;
-
-http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Bot online");
-}).listen(PORT, "0.0.0.0", () => {
-  console.log("Health server listening on", PORT);
-});
